f85f0db62f1355f67d4c8d0f44ba030ea3d9475b
ShaunK4 <113543081+ShaunK4@users.noreply.github.com>
2023-07-05 15:28:35 -0400

    Initial Setup
    Initial setup for Spin4Din utilizing Vite + React + TypeScript

---
+++ Spin4Din/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Spin4Din</h1>
+      </header>
+      <main>
+        {/* Recipe display component and other content will fo here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
